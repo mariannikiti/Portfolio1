17ed46c21ecc3e208e126093b34f66fca5703ab4
mariannikiti <102421006+mariannikiti@users.noreply.github.com>
2023-03-09 20:44:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+$(function () {
+    $('.header__btn').on('click', function () {
+        $('.rightsite-menu').removeClass('rightsite-menu--close');
+    });
+    $('.rightsite-menu__close').on('click', function () {
+        $('.rightsite-menu').addClass('rightsite-menu--close');
+    });
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+
+    var mixer = mixitup('.gallery__inner',{
+        load: {
+            filter: '.living'
+        }
+    }
+    )
+})
